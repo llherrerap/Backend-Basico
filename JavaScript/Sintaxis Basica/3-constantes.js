66677364ed911a4862df9cd41406a262d10065e1
llherrerap <llherrerap@correo.udistrital.edu.co>
2023-01-09 13:47:53 -0500

    Constantes en JavaScript

---
+++ JavaScript/Sintaxis Basica/3-constantes.js
@@ -0,0 +1,9 @@
+/*Constantes.
+Son datos que no cambian en la ejecución del código, se definen de la siguiente manera.
+    const NOMBRE_CONSTANTE = valorConstante
+Por ejemplo, en mátematicas el valor de PI no cambia, es cosntante. 
+Vea el siguiente código*/
+
+const PI=3.1416
+//Otra manera de concatenar texto es: `Texto ${variable / constante}`
+console.log(`El valor de PI es: ${PI}`)
