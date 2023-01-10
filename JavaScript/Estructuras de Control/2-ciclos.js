e75392b75404f5aa87ed12c30f7c439d2adde979
llherrerap <llherrerap@correo.udistrital.edu.co>
2023-01-09 20:02:44 -0500

    Estructuras de control ciclicas

---
+++ JavaScript/Estructuras de Control/2-ciclos.js
@@ -0,0 +1,26 @@
+/*While.
+Permite ejecutar un código mientras se cumpla una condición.
+Como se ve a continuación*/
+
+console.log("-----------------Condicional while----------------------")
+
+let cont=0
+
+/*La variable cont aumenta en uno. Sigue siempre y cuando el contador sea menor que 10*/
+
+while(cont<10){
+    console.log(`El while va en ${cont}`)
+    cont++
+}
+
+/*For
+Permite ejecutar un código un número determinado de veces.
+Como se ve a continuación*/
+
+/*La variable i aumenta de uno en uno siempre que sea mejor que 10*/
+
+console.log("-----------------Condicional for----------------------")
+
+for (let i = 0; i < 10; i++) {
+    console.log(`El for va en ${i}`)
+}
