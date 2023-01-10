20aa14d5c6a370785f915c7c06cfb159d28849b2
llherrerap <llherrerap@correo.udistrital.edu.co>
2023-01-09 21:10:06 -0500

    Funciones anonimas

--- JavaScript/Funciones/2-funcionesExpresadas.js
+++ JavaScript/Funciones/2-funcionesExpresadas.js
@@ -1,8 +1,24 @@
-/*Funciones expresadas
+/*Funciones expresadas o anonimas.
 Las funciones expresadas hacen parte del ámbito concreto del programa, es decir, estas funciones no existen hasta que la expresión que las genera es ejecutada. Se consideran una buena practica de programación para tener el código ordenado*/
 
+console.log("--------------------Función Expresada-----------------------");
 const funcionExpresada = function(){
     return console.log("Esta es una función expresada!");
 }
 
 funcionExpresada();
+
+/*Funciones anonimas autoejecutables (IIFE).
+Son funciones que se ejecutan al momento de crearlas. Su sintaxis es la siguiente:
+(function (parametros){//contenido de la funcion})(parametros);
+*/
+
+console.log("----------Funciones anonimas autoejecutables (IIFE)-------------");
+
+(function (){
+    console.log("Mi primera funcion anonima autoejecutable (IIFE)");
+})();
+
+(function (c){
+    c.log("Mi segunda funcion anonima autoejecutable (IIFE) con parametros");
+})(console);
