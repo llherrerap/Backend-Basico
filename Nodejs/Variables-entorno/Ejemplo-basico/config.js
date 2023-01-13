43d78cdff5ab7ee75a4c3c97f4b5708e64164b6a
Ana Garcia <ganamaa@hotmail.es>
2023-01-13 11:03:51 -0500

    Ejemplo basico, variables de entorno en nodejs

---
+++ Nodejs/Variables-entorno/Ejemplo-basico/config.js
@@ -0,0 +1,6 @@
+// config.js
+module.exports = {
+    NODE_ENV: process.env.NODE_ENV || 'desarrollo',
+    HOST: process.env.HOST || '127.0.0.1',//localhot -- puede poner la ip de su computadora
+    PORT: process.env.PORT || 3000
+}
---
+++ Nodejs/Variables-entorno/Ejemplo-basico/index.js
@@ -0,0 +1,17 @@
+//servidor
+// server.js
+const config = require('./config.js');
+const express = require('express');
+const app = express();
+
+console.log(`NODE_ENV=${config.NODE_ENV}`);
+
+app.get('/', (req, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    res.send("<h1>Hola mundo!</h1>");
+});
+
+app.listen(config.PORT, config.HOST, function () {
+    console.log(`El servidor se esta ejecutando en http://${config.HOST}:${config.PORT}`);
+});
